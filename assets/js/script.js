c3cabf257a50f3bc651fe7feb83ff231911534db
Derek Medrano <derekjmedrano@gmail.com>
2023-12-29 17:07:30 -0600

    fixed APIKey and started eventlistener function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
-var APIKey = "eee1f9e12d707b9e8ff0fa060b7d7705";
+var APIKey = "e87c58f5514b8732b0b47d1ae49b49a36";
+// var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}"
+
 var cityName = $('#city-name');
+var searchBtn = $('#search-button');
+
+searchBtn.on('click', function(event) {
+    preventDefault(event);
+    var cityNameInput = cityName.value;
+
+    // if (cityNameInput !=== )
+
+
+})
+
+var getGeo = function(city) {
+
+}
