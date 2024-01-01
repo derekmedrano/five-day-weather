8f11874640d4d00c01362f03871fc0844d1aea9c
Derek Medrano <derekjmedrano@gmail.com>
2024-01-01 14:40:18 -0600

    fixed user input, need to get lat and lon now

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ var apiKey = 'eee1f9e12d707b9e8ff0fa060b7d7705';
 var countryCode = 'US';
 
 var cityInput = $('#city');
-var cityName = cityInput.val();
 var searchBtn = $('#search-button');
 
 
 $(searchBtn.on('click', function(event) {
     event.preventDefault();
+    var cityName = cityInput.val();
 
     console.log(cityName)
     getGeo(cityName);
@@ -18,7 +18,7 @@ $(searchBtn.on('click', function(event) {
 
 function getGeo(city) {
     var geoURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + ',' +countryCode + '&limit=5&appid=' + apiKey
-
+    console.log(city);
     fetch(geoURL)
         .then(function (response) {
         return response.json();
