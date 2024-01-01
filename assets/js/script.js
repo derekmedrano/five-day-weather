789bc1a3617580da5540893a6efffc1f1d749acb
Derek Medrano <derekjmedrano@gmail.com>
2024-01-01 12:40:00 -0600

    added some comments as a framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,36 @@
-var apiKey = "51d4821afd708744cf080bebb2d5390a";
+var apiKey = 'eee1f9e12d707b9e8ff0fa060b7d7705';
+var countryCode = 'US';
 
 var cityInput = $('#city');
 var searchBtn = $('#search-button');
+var cityInputVal = cityInput.val();
 
 $(searchBtn.on('click', function(event) {
     event.preventDefault();
 
-    var cityInputVal = cityInput.val();
     console.log(cityInputVal)
     getGeo(cityInputVal);
 
 }));
 
+// func() to check storage and add any previous searches as buttons to the aside
+
 function getGeo(city) {
-    var geoURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey
+    var geoURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + ',' +countryCode + '&limit=5&appid=' + apiKey
 
     fetch(geoURL)
         .then(function (response) {
         return response.json();
     })
     .then(function(data) {
-        console.log(data)
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        console.log('latitude=' + lat + 'longitude=' + lon);
     });
-};
+}; 
+
+// func() to get current weather using getGeo()
+    //add appropriate icons
+//func() to get 5 day weather forecast using getGeo()
+
+//func() to store it in local storage
