5ad455d2e8ccd58fa039ebfcbf893927b4b65806
Derek Medrano <derekjmedrano@gmail.com>
2024-01-02 15:04:58 -0600

    session with tutor, big commit but will add comments in future to help readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,17 @@ $(searchBtn.on('click', function(event) {
 
     console.log(cityName)
     getGeo(cityName);
+    storeSearch(cityName);
 
 }));
+
+function storeSearch (cityName) {
+    var history = JSON.parse(localStorage.getItem('cities')) || [];
+    if (!history.includes(cityName)) {
+        history.push(cityName)
+        localStorage.setItem('cities', JSON.stringify(history));
+    }
+}
 
 // ADD func() to check storage and add any previous searches as buttons to the aside
 
@@ -25,15 +34,44 @@ function getGeo(city) {
     })
     .then(function(data) {
         console.log(data);
-        // var lat = data.list[0].coord.lat;
-        // var lon = data.list[0].coord.lon;
-        // console.log('latitude=' + lat + 'longitude=' + lon);
+        currentWeather(data.list[0]);
+        fiveDay(data.list);
+
     });
 }; 
 
-// ADD func() to get current weather using getGeo()
-    //add appropriate icons
+function currentWeather(currentDay) {
+    var cityName = cityInput.val();
+    $('#city-name').text(cityName);
+    $('#temp').text('Tempurature:' + currentDay.main.temp);
+    $('#humidity').text('Humidity:' + currentDay.main.humidity);
+    $('#wind').text('Wind:' + currentDay.wind.speed);
 
-// ADD func() to get 5 day weather forecast using getGeo()
+}
 
-// ADD func() to store it in local storage
+function fiveDay(list) {
+ for(var i = 7; i < list.length; i+=8) {
+    var day = list[i];
+    var card = $('<div></div>')
+    var date = $('<p></p>');
+    var temp = $('<p></p>');
+    var humidity = $('<p></p>');
+    var wind = $('<p></p>');
+    
+    date.text(dayjs.unix(day.dt).format("MM-DD-YYYY"));
+    temp.text(day.main.temp);
+    humidity.text(day.main.humidity);
+    wind.text(day.wind.speed);
+    $(card).append(date);
+    $(card).append(temp);
+    $(card).append(humidity);
+    $(card).append(wind);
+
+    
+
+
+    $('#five-day').append(card);
+ }
+}
+
+
